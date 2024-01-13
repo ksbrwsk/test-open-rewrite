16019b188d668a0bc3323b14456476a3d68b4190
Kai Saborowski <kai.saborowski@googlemail.com>
2024-01-13 16:19:26 +0100

    Initial commit.

---
+++ src/test/java/de/ksbrwsk/openrewrite/TestOpenrewriteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ksbrwsk.openrewrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOpenrewriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
